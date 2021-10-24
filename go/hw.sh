c4db79b99e4f6040c328ec54b9b05b1db868c137
Naga Vijayapuram <nvijayap@gmail.com>
2021-10-24 09:04:53 -0700

    go

---
+++ go/hw.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+OS=`uname -s`
+
+if [ "$OS" == "Darwin" ]; then
+  echo -e "\n========\nsw_vers\n========\n"
+  sw_vers
+elif [ "$OS" == "Linux" ]; then
+  echo -e "\nuname -a\n"
+  uname -a
+  echo -e "\ncat /etc/os-release\n"
+  cat /etc/os-release
+fi
+
+echo -e "\n==================\ntime go run hw.go\n==================\n"
+time go run hw.go
+
+echo
